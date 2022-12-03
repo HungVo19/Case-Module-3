393aa8f392f6486e42215d09387c84ddc1c7237e
DienVuong <vuongdien261@gmail.com>
2022-12-03 09:38:13 +0700

    add model

---
+++ src/main/java/com/example/online_electronics_store/model/OrderDetails.java
@@ -0,0 +1,40 @@
+package com.example.online_electronics_store.model;
+
+public class OrderDetails {
+    private User user;
+    private Product product;
+    private Integer quantity;
+
+    public OrderDetails() {
+    }
+
+    public OrderDetails(User user, Product product, Integer quantity) {
+        this.user = user;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
--- src/main/java/com/example/online_electronics_store/model/Product.java
+++ src/main/java/com/example/online_electronics_store/model/Product.java
@@ -1,4 +1,92 @@
 package com.example.online_electronics_store.model;
 
 public class Product {
+   private Long id;
+   private String name;
+   private Double price;
+   private String description;
+   private String image;
+   private Boolean stockStatus;
+   private Boolean status;
+   private Category categories;
+
+   public Product() {
+   }
+
+   public Product(Long id, String name, Double price, String description, String image, Boolean stock_status, Boolean status, Category categories) {
+      this.id = id;
+      this.name = name;
+      this.price = price;
+      this.description = description;
+      this.image = image;
+      this.stockStatus = stock_status;
+      this.status = status;
+      this.categories = categories;
+   }
+
+
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Double getPrice() {
+      return price;
+   }
+
+   public void setPrice(Double price) {
+      this.price = price;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public String getImage() {
+      return image;
+   }
+
+   public void setImage(String image) {
+      this.image = image;
+   }
+
+   public Boolean getStock_status() {
+      return stockStatus;
+   }
+
+   public void setStock_status(Boolean stock_status) {
+      this.stockStatus = stock_status;
+   }
+
+   public Boolean getStatus() {
+      return status;
+   }
+
+   public void setStatus(Boolean status) {
+      this.status = status;
+   }
+
+   public Category getCategories() {
+      return categories;
+   }
+
+   public void setCategories(Category categories) {
+      this.categories = categories;
+   }
 }
